2fa1cb66ce9a18792ae6d84eb4bb03e99cdb086a
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2026-05-24 00:11:26 +0300

    Delete src directory

--- src/service/deleteMovie.js
+++
@@ -1,9 +0,0 @@
-import { db } from '@/service/sdk'
-import { doc, deleteDoc } from "firebase/firestore";
-
-const deleteData = async (key) => {
-  await deleteDoc(doc(db, "privateMovies", key));
-} 
-
-
-export default deleteData
